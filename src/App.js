31963976968d74f46d08477bca3a888244b0492d
unknown <30684@fiecalunos.lan>
2020-02-12 21:34:25 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import Routes from "./routes";
 
 function App() {
-  return <Routes />;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h3>Guarani 1 x 0 Corinthians</h3>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 const startApp = () => {
-  ReactDOM.render(<App />, document.getElementById("root"));
-  serviceWorker.unregister();
-};
+    ReactDOM.render(<App />, document.getElementById('root'));
+    serviceWorker.unregister();
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
-if (!window.cordova) {
-  startApp();
+
+if(!window.cordova) {
+    startApp();
 } else {
-  document.addEventListener("deviceready", startApp, false);
+    document.addEventListener('deviceready', startApp, false);
 }
+
